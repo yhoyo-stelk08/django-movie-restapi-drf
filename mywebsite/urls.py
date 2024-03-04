8c10cdacc05dfcd836cd0f8317460fa39b15b07e
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-04 18:04:28 +0800

    feat: add new apps named movies and make necessary changes, add some lines in .gitignore

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index,name="index"),
     path('admin/', admin.site.urls),
-    path('api/',include('drf.urls',namespace='api'))
-]
+    path('api/',include('drf.urls',namespace='api')),
+    path('movies/',include('movies.urls',namespace='movies')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
