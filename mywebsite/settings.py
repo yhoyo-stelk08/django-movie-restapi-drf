b000c0c74d0ea4326f9d6d88e496490e979af3a1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-04 11:52:10 +0800

    feat : modified settings.py add MEDIA_ROOT and MEDIA_URL

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'pictures')
+MEDIA_URL='/pictures/'
