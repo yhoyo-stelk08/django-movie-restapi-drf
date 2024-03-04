212ce317564f6077bf835a1b77cae996194fe7f3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-04 13:41:33 +0800

    feat: adding ALLOWED_HOSTS in settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w9b6nfszqvv41ujm!_wg$33wr=lhl-3s6)(x$7q49@ei(#z%%#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost','127.0.0.1']
 
 
 # Application definition
