4dc503e2caf9d320a63702d6d5cce1e3f36d0c16
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-04 12:06:32 +0800

    feat : modified some and adding new files modified:   .gitignore modified:   drf/admin.py modified:   drf/models.py new file:   drf/urls.py

--- drf/models.py
+++ drf/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model) :
+    title = models.CharField(max_length=255,null=False,blank=False)
+    desc = models.TextField(blank=True)
+    rating = models.FloatField()
+    duration = models.CharField(max_length=30)
